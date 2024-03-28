75c0cd792aba37eb9cf9084c139c055246e79a15
Gustavo <132701636+ByGustavoo@users.noreply.github.com>
2024-03-27 21:51:23 -0300

    CRUD.sql

--- CRUD.sql
+++ CRUD.sql
@@ -1,22 +1,25 @@
 --> Criando a tabela 'aluno'.
 
 create table aluno (
-    id serial, --> Serial, tem auto incremento automático.
-    nome varchar(255), --> Varchar, usamos quando temos um tamanho já predeterminado.
-    cpf char(11), --> Char, usamos quando sabemos exatamento o tamanho que iremos utilizar.
-    observacao text, --> Text, usamos quando não sabemos o tamanho do texto.
-    idade int, --> Int, número inteiro.
-    dinheiro numeric(10,2),--> Numeric, conseguimos definir quantas casas decimais queremos, '12345678,90'
-    altura real, --> Real, quando precisamos usar casas decimais de até 6 dígitos com precisão.
-    ativo boolean, --> Boolean, verdadeiro ou falso.
-    data_nascimento date, --> Date, estabelece a data, sempre nessa ordem: 'YYYY-MM-DD' ou seja, '2005-05-19'.
-    hora_aula time, --> Time, estabelece a hora, sempre nessa ordem: 'HH24:MI:SS', ou seja, '17:19:00'.
-    matricula_data timestamp --> Timestamp, representa data e hora, '2005-05-19 17:19:00'.
+    id serial,                	--> Serial, tem auto incremento automático.
+    nome varchar(255),        	--> Varchar, usamos quando temos um tamanho já predeterminado.
+    cpf char(11),             	--> Char, usamos quando sabemos exatamento o tamanho que iremos utilizar.
+    observacao text,          	--> Text, usamos quando não sabemos o tamanho do texto.
+    idade int,                	--> Int, número inteiro.
+    dinheiro numeric(10,2),   	--> Numeric, conseguimos definir quantas casas decimais queremos, '12345678,90'
+    altura real,              	--> Real, quando precisamos usar casas decimais de até 6 dígitos com precisão.
+    ativo boolean,           	 --> Boolean, verdadeiro ou falso.
+    data_nascimento date,     	--> Date, estabelece a data, sempre nessa ordem: 'YYYY-MM-DD' ou seja, '2005-05-19'.
+    hora_aula time,           	--> Time, estabelece a hora, sempre nessa ordem: 'HH24:MI:SS', ou seja, '17:19:00'.
+    matricula_data timestamp  	--> Timestamp, representa data e hora, '2005-05-19 17:19:00'.
 );
+
 
 --> Inserindo dados na tabela 'aluno'.
 
-insert into aluno(nome, --> Passamos o nome da tabela, 'aluno', e os campos que queremos preencher.
+Passamos o nome da tabela, 'aluno', e os campos que queremos preencher.
+
+insert into aluno(nome,
     cpf,
     observacao,
     idade,
@@ -27,7 +30,10 @@ insert into aluno(nome, --> Passamos o nome da tabela, 'aluno', e os campos que 
     hora_aula,
     matricula_data
 )
-    values('Guru', --> Passamos os valores dos campos, já com os valores preenchidos na ordem correta.
+
+Passamos os valores dos campos, já com os valores preenchidos na ordem correta.
+	
+    values('Guru', 
     '99999999999',
     'Guru do Himalaia é fera',
     18,
@@ -39,28 +45,28 @@ insert into aluno(nome, --> Passamos o nome da tabela, 'aluno', e os campos que 
     '2023-05-27 22:30:00' 
 );
 
+
 --> Consultando os dados da tabela 'aluno'.
 
-select * from aluno --> Usamos o (*), quando queremos trazer todas as informações da tabela 'aluno'.
+Usamos o (*), quando queremos trazer todas as informações da tabela 'aluno'.
 
+select * from aluno 
 
---> Para atualizar, passamos o nome da tabela 'aluno', o campo que será atualizado, utilizando o 'set', 
---> o campo 'dinheiro', o valor '1000.90' e estabelecemos qual dado será atualizado por meio do 
---> 'where' e do 'id'.
-
+	
+Para atualizar, passamos o nome da tabela 'aluno', o campo que será atualizado, utilizando o 'set', 
+o campo 'dinheiro', o valor '1000.90' e estabelecemos qual dado será atualizado por meio do 
+'where' e do 'id'.
 
 update aluno set dinheiro = 1000.90 where id = 1
 	
 	
---> Para deletar um dado, passamos o nome da tabela 'aluno', e estabelecemos qual dado será deletado
---> por meio do 'where' e do 'id', nesse caso irá deletar todas as informações do aluno de id = 1.
-
+Para deletar um dado, passamos o nome da tabela 'aluno', e estabelecemos qual dado será deletado
+por meio do 'where' e do 'id', nesse caso irá deletar todas as informações do aluno de id = 1.
 
 delete from aluno where id = 1
 
 
---> Ao invés de retornar todos os campos utilizando o (*), podemos passar o nome das colunas que 
---> queremos retornar no select, passando o nome de cada uma delas.
-
-
+Ao invés de retornar todos os campos utilizando o (*), podemos passar o nome das colunas que 
+queremos retornar no select, passando o nome de cada uma delas.
+	
 select nome, cpf, data_nascimento from aluno;
