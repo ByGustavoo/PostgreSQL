0855efff7a5b8988fecec8656704e12a77d7f24e
Gustavo <132701636+ByGustavoo@users.noreply.github.com>
2024-04-01 21:59:35 -0300

    Funções de Agregação.sql

--- Funções de Agregação.sql
+++ Funções de Agregação.sql
@@ -10,27 +10,28 @@ CREATE TABLE funcionarios(
 
 -- Inserção de registros na tabela 'funcionarios'
 
-INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M001', 'Diogo', 'Mascarenhas');    -- Insere um funcionário com matrícula 'M001', nome 'Diogo' e sobrenome 'Mascarenhas'
-INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M002', 'Vinícius', 'Dias');        -- Insere um funcionário com matrícula 'M002', nome 'Vinícius' e sobrenome 'Dias'
-INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M003', 'Nico', 'Steppat');         -- Insere um funcionário com matrícula 'M003', nome 'Nico' e sobrenome 'Steppat'
-INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M004', 'João', 'Roberto');         -- Insere um funcionário com matrícula 'M004', nome 'João' e sobrenome 'Roberto'
-INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M005', 'Diogo', 'Mascarenhas');    -- Insere um funcionário com matrícula 'M005', nome 'Diogo' e sobrenome 'Mascarenhas'
-INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M006', 'Alberto', 'Martins');      -- Insere um funcionário com matrícula 'M006', nome 'Alberto' e sobrenome 'Martins'
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M001', 'Diogo', 'Mascarenhas');    -- Insere um funcionário 
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M002', 'Vinícius', 'Dias');        -- Insere um funcionário 
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M003', 'Nico', 'Steppat');         -- Insere um funcionário 
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M004', 'João', 'Roberto');         -- Insere um funcionário 
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M005', 'Diogo', 'Mascarenhas');    -- Insere um funcionário 
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M006', 'Alberto', 'Martins');      -- Insere um funcionário
 
 
--- COUNT   -  Retorna a quantidade de registros
--- SUM 	   -   Retorna a soma dos registros
--- MAX 	   -   Retorna o maior valor dos registros
--- MIN 	   -   Retorna o menor valor dos registros
--- AVG 	   -   Retorna a média dos registros
+-- COUNT    -  Retorna a quantidade de registros
+-- SUM 	    -  Retorna a soma dos registros
+-- MAX 	    -  Retorna o maior valor dos registros
+-- MIN 	    -  Retorna o menor valor dos registros
+-- AVG 	    -  Retorna a média dos registros
 
 
--- Retorna o número total de registros na tabela 'funcionarios', (COUNT), retorna a soma de todos os valores do ID da tabela 'funcionarios', (SUM), retorna o valor máximo do ID na tabela 
--- 'funcionarios', (MAX), retorna o valor mínimo do ID na tabela 'funcionarios', (MIN), retorna a média arredondada para o número inteiro mais próximo do ID na tabela 'funcionarios', dividindo o 'SUM' com o 'COUNT'
+-- Retorna o número total de registros na tabela 'funcionarios', (COUNT), retorna a soma de todos os valores do ID da tabela 'funcionarios', (SUM), 
+-- retorna o valor máximo do ID na tabela 'funcionarios', (MAX), retorna o valor mínimo do ID na tabela 'funcionarios', (MIN), retorna a média 
+-- arredondada com duas casas decimais, do ID na tabela 'funcionarios', dividindo o 'SUM' com o 'COUNT'.
 
 SELECT COUNT(id) AS total_registros,
        SUM(id) AS soma_ids,
        MAX(id) AS max_id,
        MIN(id) AS min_id,
-       ROUND(AVG(id), 0) AS media_id_arredondada
+       ROUND(AVG(id), 2) AS media_id_arredondada
   FROM funcionarios;
